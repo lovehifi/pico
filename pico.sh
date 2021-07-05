751715a711e8eadb373043526dbeadd4cef4d2f4
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-07-05 08:46:35 +0700

    Add files via upload

--- pico.sh
+++ pico.sh
@@ -3,25 +3,20 @@
 . /etc/init.d/tc-functions
 . /var/www/cgi-bin/pcp-functions
 useBusybox
-#TARGET=`cat /etc/sysconfig/backup_device`
-#cd /mnt/$TARGET
+TARGET=`cat /etc/sysconfig/backup_device`
+cd /mnt/$TARGET
 #echo pcp-aplayer_render.tcz >>onboot.lst
-#cd optional
+cd optional
 #wget https://raw.githubusercontent.com/sam0402/piCorePlayer-Album_Player/master/pcp-aplayer_render.tcz
-#pcp_write_var_to_config USER_COMMAND_2 "aplayer;aprenderer"
-
-cd /tmp
-wget https://raw.githubusercontent.com/lovehifi/pico/main/MaterialSkin.tar.gz
 wget https://raw.githubusercontent.com/lovehifi/pico/main/pcp-7.0.0-www.tcz
 wget https://raw.githubusercontent.com/lovehifi/pico/main/slimserver.tcz
+cd /tmp
+wget https://raw.githubusercontent.com/lovehifi/pico/main/MaterialSkin.tar.gz
 wget https://raw.githubusercontent.com/lovehifi/pico/main/pcpm.tar.gz
-
-tar -zxf /tmp/MaterialSkin.tar.gz --overwrite -C /
-tar -zxf /tmp/pcp-7.0.0-www.tcz --overwrite -C /
-tar -zxf /tmp/slimserver.tcz --overwrite -C /
-tar -zxf /tmp/pcpm.tar.gz --overwrite -C /
-
+tar -zxf MaterialSkin.tar.gz --overwrite -C /
+tar -zxf pcpm.tar.gz --overwrite -C /
 pcp_write_var_to_config USER_COMMAND_3 "/mnt/mmcblk0p2/tce/pcpm.sh"
+#pcp_write_var_to_config USER_COMMAND_2 "aplayer;aprenderer"
 echo "Rebooting..."
 sleep 3
 pcp br
