ac20bf2b93f36a8db57a90d9c11494befe6ae7d9
lovehifi <81028393+lovehifi@users.noreply.github.com>
2021-07-05 12:10:06 +0700

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -13,15 +13,17 @@ wget https://raw.githubusercontent.com/lovehifi/pico/main/pcp-7.0.0-www.tcz
 wget https://raw.githubusercontent.com/lovehifi/pico/main/slimserver.tcz
 wget https://raw.githubusercontent.com/lovehifi/pico/main/MaterialSkin.tar.gz
 wget https://raw.githubusercontent.com/lovehifi/pico/main/pcpm.tar.gz
+wget https://raw.githubusercontent.com/lovehifi/pico/main/ap.tar.gz
 mv /mnt/mmcblk0p2/tce/optional/pcp-7.0.0-www.tcz /mnt/mmcblk0p2/tce/optional/pcp-7.0.0-www_o.tcz
 mv /mnt/mmcblk0p2/tce/optional/slimserver.tcz /mnt/mmcblk0p2/tce/optional/slimserver_o.tcz
 tar -zxf MaterialSkin.tar.gz --overwrite -C /
 tar -zxf pcpm.tar.gz --overwrite -C /
+tar -zxf ap.tar.gz --overwrite -C /
 mv /tmp/pcp-7.0.0-www.tcz /mnt/mmcblk0p2/tce/optional/
 mv /tmp/slimserver.tcz /mnt/mmcblk0p2/tce/optional/
 pcp_write_var_to_config USER_COMMAND_3 "/mnt/mmcblk0p2/tce/pcpm.sh"
 mkdir /mnt/mmcblk0p2/tce/slimserver/prefs/material-skin
-#pcp_write_var_to_config USER_COMMAND_2 "aplayer;aprenderer"
+#pcp_write_var_to_config USER_COMMAND_2 "/home/tc/aplayer/aplayer.sh;/home/tc/aprenderer/aprenderer.sh;/home/tc/apmenu/apmenu.sh"
 echo "Rebooting..."
 sleep 3
 pcp br
